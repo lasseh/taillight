82048fa19edfaa4e204e44d4181adcc6069faa3d
Lasse <haugen.lasse@gmail.com>
2026-02-16 16:32:46 +0100

    fix: place device details link inline next to clear button

--- frontend/src/components/AppLogFilterBar.vue
+++ frontend/src/components/AppLogFilterBar.vue
@@ -58,7 +58,7 @@ const searchInput = computed({
     <RouterLink
       v-if="filterStore.filters.host"
       :to="{ name: 'applog-device-detail', params: { hostname: filterStore.filters.host } }"
-      class="text-t-teal mx-auto text-xs hover:underline"
+      class="text-t-teal text-xs hover:underline"
     >
       {{ filterStore.filters.host }} details &rarr;
     </RouterLink>
