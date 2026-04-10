17c1a040d5b1d496a0b2b9788dc7f3f41cee9265
Lasse <haugen.lasse@gmail.com>
2026-04-10 20:06:21 +0200

    fix: change seed notification rules event_kind from srvlog to netlog

--- api/migrations/000007_reference_data.up.sql
+++ api/migrations/000007_reference_data.up.sql
@@ -27,9 +27,9 @@ CREATE INDEX IF NOT EXISTS idx_juniper_ref_name ON juniper_netlog_ref (name);
 INSERT INTO notification_rules
     (name, enabled, event_kind, search, severity_max, burst_window, cooldown_seconds, max_cooldown_seconds, group_by)
 VALUES
-    ('juniper-link-down',          false, 'srvlog', 'SNMP_TRAP_LINK_DOWN',              3, 30, 60, 3600, 'hostname'),
-    ('juniper-ospf-neighbor-down', false, 'srvlog', 'RPD_OSPF_NBRDOWN',                 3, 30, 60, 3600, 'hostname'),
-    ('juniper-bgp-peer-down',     false, 'srvlog', 'RPD_BGP_NEIGHBOR_STATE_CHANGED',    4, 30, 60, 3600, 'hostname'),
-    ('juniper-chassis-alarm',      false, 'srvlog', 'CHASSISD_SNMP_TRAP',               3, 30, 60, 3600, 'hostname'),
-    ('juniper-auth-failure',       false, 'srvlog', 'SSHD_LOGIN_FAILED',                4, 30, 60, 3600, 'hostname'),
-    ('juniper-kernel-panic',       false, 'srvlog', 'KERNEL',                            2, 30, 60, 3600, 'hostname');
+    ('juniper-link-down',          false, 'netlog', 'SNMP_TRAP_LINK_DOWN',              3, 30, 60, 3600, 'hostname'),
+    ('juniper-ospf-neighbor-down', false, 'netlog', 'RPD_OSPF_NBRDOWN',                 3, 30, 60, 3600, 'hostname'),
+    ('juniper-bgp-peer-down',     false, 'netlog', 'RPD_BGP_NEIGHBOR_STATE_CHANGED',    4, 30, 60, 3600, 'hostname'),
+    ('juniper-chassis-alarm',      false, 'netlog', 'CHASSISD_SNMP_TRAP',               3, 30, 60, 3600, 'hostname'),
+    ('juniper-auth-failure',       false, 'netlog', 'SSHD_LOGIN_FAILED',                4, 30, 60, 3600, 'hostname'),
+    ('juniper-kernel-panic',       false, 'netlog', 'KERNEL',                            2, 30, 60, 3600, 'hostname');
